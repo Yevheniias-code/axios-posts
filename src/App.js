5a85bbcf409fc97f888e73efb13be0eecad1fdfb
yevheniias-code <jewgenia.lazebnyk@icloud.com>
2024-04-12 13:00:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios'
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then( res => {
+      const posts = res.data;
+      this.setState({posts})
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.posts.map(post => <p key={post.id}>{post.title}
+        <span> {post.body}</span></p>)}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
